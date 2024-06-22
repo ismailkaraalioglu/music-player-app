0cad09f40545aa936681bf2c48486ef4c31176d7
Ismail Karaalioglu <ismailkaraalioglu@yandex.com>
2024-06-22 22:58:02 +0300

    feat: Customize bottom navigation bar with blur effect and active state styling

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,20 +1,80 @@
+import {
+  FontAwesome,
+  FontAwesome6,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
 import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 
 const TabsNavigation = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "#fc3c44",
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#2a89d8",
         tabBarLabelStyle: {
+          fontSize: 12,
           fontWeight: "500",
         },
         headerShown: false,
+        tabBarStyle: {
+          position: "absolute",
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderTopWidth: 0,
+          paddingTop: 8,
+        },
+        tabBarBackground: () => (
+          <BlurView
+            intensity={95}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: "hidden",
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          />
+        ),
       }}
     >
-      <Tabs.Screen name="favorites" />
-      <Tabs.Screen name="playlists" />
-      <Tabs.Screen name="(songs)" />
-      <Tabs.Screen name="artists" />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="heart" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="playlists"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="playlist-play"
+              size={28}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="(songs)"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="musical-notes-sharp" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="artists"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="users-line" size={20} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
