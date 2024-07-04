ff785d2e04daade1252f1b1de4981f359baab90b
Ismail Karaalioglu <ismailkaraalioglu@yandex.com>
2024-07-04 17:21:15 +0300

    feat: complete favorites page UI and list favorite songs

--- src/app/(tabs)/favorites/index.tsx
+++ src/app/(tabs)/favorites/index.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import TRACKS from "@/assets/data/library.json";
+import TrackList from "@/components/track-list";
+import useNavigationSearch from "@/hooks/use-navigation-search";
+import { useMemo } from "react";
+import { ScrollView, View } from "react-native";
 
 const FavoritesScreen = () => {
+  const search = useNavigationSearch({
+    searchBarOptions: {
+      placeholder: "Find in songs",
+    },
+  });
+
+  const favoriteTracks = useMemo(() => {
+    const filteredTracks = TRACKS.filter(
+      (track) =>
+        track.rating === 1 &&
+        track.title?.toLowerCase().includes(search.toLowerCase())
+    );
+    return filteredTracks;
+  }, [search]);
+
   return (
     <View className="flex-1 bg-black">
-      <Text className="text-xl text-black">Favorites Screen</Text>
+      <ScrollView contentInsetAdjustmentBehavior="automatic" className="px-6">
+        <TrackList tracks={favoriteTracks} scrollEnabled={false} />
+      </ScrollView>
     </View>
   );
 };
